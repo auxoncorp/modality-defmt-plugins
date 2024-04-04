c8f4fd2f7dca7c450c1dfc32aa9301924cf4c677
Jon Lamb <jon@auxon.io>
2024-04-04 09:34:56 -0700

    Bump version, update robot framework script

--- test_system/scripts/run_robot_framework.sh
+++ test_system/scripts/run_robot_framework.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-renode-test --kill-stale-renode-instances --variable CREATE_SNAPSHOT_ON_FAIL:False tests.robot
+renode-test --jobs 1 --kill-stale-renode-instances --variable CREATE_SNAPSHOT_ON_FAIL:False tests.robot
 
 exit 0
